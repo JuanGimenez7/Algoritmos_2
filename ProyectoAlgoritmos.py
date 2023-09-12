b82e915d6a2d7eea1ada92c1723ad8570334b6b9
Juan <jgimenezeee@gmail.com>
2023-09-12 16:26:03 -0400

    Nuevo cambio de Neil

--- ProyectoAlgoritmos.py
+++ ProyectoAlgoritmos.py
@@ -1,125 +1,192 @@
 import csv
 import json
-from datetime import datetime
+import heapq
 
-print("Este es un mensaje nuevo")
-#CREACION DE CLASE RESERVACION
 class Reservacion:
-#CONSTRUCTOR
-    def __init__(self, nombre_cliente, fecha_reserva, fecha_entrada, fecha_salida, num_habitacion, duracion_estadia, tipo_habitacion, preferencias_alimentarias, num_personas, contacto, precio_total, metodo_pago, notas_adicionales, estado_reservacion, hora_checkin_checkout, id_reservacion, correo_cliente):
+    def __init__(self, nombre_cliente, fecha_reserva, fecha_entrada, fecha_salida, numero_habitacion, duracion_estadia, tipo_habitacion, preferencias_alimentarias, numero_personas, contacto, precio_total, metodo_pago, notas_adicionales, estado_reservacion, hora_check_in_out, id_reservacion, correo_cliente):
         self.nombre_cliente = nombre_cliente
         self.fecha_reserva = fecha_reserva
         self.fecha_entrada = fecha_entrada
         self.fecha_salida = fecha_salida
-        self.num_habitacion = num_habitacion
+        self.numero_habitacion = numero_habitacion
         self.duracion_estadia = duracion_estadia
         self.tipo_habitacion = tipo_habitacion
         self.preferencias_alimentarias = preferencias_alimentarias
-        self.num_personas = num_personas
+        self.numero_personas = numero_personas
         self.contacto = contacto
         self.precio_total = precio_total
         self.metodo_pago = metodo_pago
         self.notas_adicionales = notas_adicionales
         self.estado_reservacion = estado_reservacion
-        self.hora_checkin_checkout = hora_checkin_checkout
+        self.hora_check_in_out = hora_check_in_out
         self.id_reservacion = id_reservacion
         self.correo_cliente = correo_cliente
 
-#CREACION DE CLASE SISTEMA RESERVACIONES
-class SistemaReservaciones:
-#CONSTRUCTOR
+
+class Funciones:
+
+    def listar3(self):
+        for reservacion in self.reservaciones:
+            print(reservacion.nombre_cliente,reservacion.fecha_entrada, reservacion.fecha_salida, reservacion.precio_total)
+
+    def listar4(self):
+        for reservacion in self.reservaciones:
+            print(reservacion.nombre_cliente,reservacion.fecha_entrada, reservacion.fecha_salida, reservacion.precio_total)
+    
+    def listar5(self):
+        for reservacion in self.reservaciones:
+            print(reservacion.nombre_cliente, reservacion.duracion_estadia)
+            
     def __init__(self):
         self.reservaciones = []
+        
+    def cargar_reservaciones_desde_csv(self, archivo):
+        self.reservaciones = []
+        with open(archivo, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                reservacion = Reservacion(
+                    row["Nombre del Cliente"],
+                    row["Fecha de Reserva"],
+                    row["Fecha de Entrada"],
+                    row['Fecha de Salida'],
+                    row['Número de Habitación'],
+                    int(row['Duración de la Estadía']),
+                    row['Tipo de Habitación'],
+                    row['Preferencias Alimentarias'],
+                    int(row['Número de Personas']),
+                    row['Contacto'],
+                    float(row['Precio Total']),
+                    row['Método de Pago'],
+                    row['Notas Adicionales'],
+                    row['Estado de la Reservación'],
+                    row['Hora de Check-in/Check-out'],
+                    row['ID de Reservación'],
+                    row['Correo del Cliente']
+                )
+                self.reservaciones.append(reservacion)
+        return self.reservaciones
 
-#FUNCION CARGAR RESERVACIONES PARA ARCHIVOS .csv o .json
-    def cargar_datos(self, archivo):
-        if archivo.endswith('.csv'):
-            with open(archivo, 'r') as file:
-                reader = csv.DictReader(file)
-                for row in reader:
-                    reservacion = Reservacion(
-                        row['Nombre Cliente'],
-                        datetime.strptime(row['Fecha Reserva'], '%Y-%m-%d'),
-                        datetime.strptime(row['Fecha Entrada'], '%Y-%m-%d'),
-                        datetime.strptime(row['Fecha Salida'], '%Y-%m-%d'),
-                        int(row['Número de Habitación']),
-                        int(row['Duración de Estadía']),
-                        row['Tipo de Habitación'],
-                        row['Preferencias Alimentarias'],
-                        int(row['Número de Personas']),
-                        row['Contacto'],
-                        float(row['Precio Total']),
-                        row['Método de Pago'],
-                        row['Notas Adicionales'],
-                        row['Estado de la Reservación'],
-                        row['Hora de Check-in/Check-out'],
-                        int(row['ID de Reservación']),
-                        row['Correo del Cliente']
-                    )
-                    self.reservaciones.append(reservacion)
-        elif archivo.endswith('.json'):
-            with open(archivo, 'r') as file:
-                data = json.load(file)
-                for item in data:
-                    reservacion = Reservacion(
-                        item['nombre_cliente'],
-                        datetime.strptime(item['fecha_reserva'], '%Y-%m-%d'),
-                        datetime.strptime(item['fecha_entrada'], '%Y-%m-%d'),
-                        datetime.strptime(item['fecha_salida'], '%Y-%m-%d'),
-                        int(item['num_habitacion']),
-                        int(item['duracion_estadia']),
-                        item['tipo_habitacion'],
-                        item['preferencias_alimentarias'],
-                        int(item['num_personas']),
-                        item['contacto'],
-                        float(item['precio_total']),
-                        item['metodo_pago'],
-                        item['notas_adicionales'],
-                        item['estado_reservacion'],
-                        item['hora_checkin_checkout'],
-                        int(item['id_reservacion']),
-                        item['correo_cliente']
-                    )
-                    self.reservaciones.append(reservacion)
+    def cargar_reservaciones_desde_json(self, archivo):
+        self.reservaciones = []
+        with open(archivo, 'r') as file:
+            data = json.load(file)
+            for item in data:
+                reservacion = Reservacion(
+                    item['nombre_cliente'],
+                    item['fecha_reserva'],
+                    item['fecha_entrada'],
+                    item['fecha_salida'],
+                    item['num_habitacion'],
+                    item['duracion_estadia'],
+                    item['tipo_habitacion'],
+                    item['preferencias_alimentarias'],
+                    item['num_personas'],
+                    item['contacto'],
+                    item['precio_total'],
+                    item['metodo_pago'],
+                    item['notas_adicionales'],
+                    item['estado_reservacion'],
+                    item['hora_checkin_checkout'],
+                    item['id_reservacion'],
+                    item['correo_cliente']
+                )
+                self.reservaciones.append(reservacion)
+        return self.reservaciones
 
-#Ordenar segun criterios de ordenamiento dados por el usuario//metodo quicksort
-    def ordenar_reservaciones(self, criterios):
-        return 
-#Ordenamiento Multiple segun criterios dados por el usuario        
-    def ordenar_reservaciones_multiple(self, criterios):
-        return
-#Ordenamiento segun el precio total de forma ascendente o descendente(Solo de las reservaciones que caigan en el rango de las fechas que el usuario da)//metodo mergesort
-    def ordenar_reservaciones_por_rango(self, fecha_inicio, fecha_fin, ascendente=True):
-        print("precio_total")
 
-#Ordenamiento segun las reservaciones que tenga el usuario //metodo Shellsort 
-    def listar_clientes_por_reservaciones(self, ascendente=True):
-        return
+#METODO HEAPSORT PARA ORDENAR DE MANERA ASCENDENTE LAS RESERVACIONES SEGUN SU DURACION DE ESTADIA
+    def heapifyascendente(self, arr, n, i):
+        largest = i
+        l = 2 * i + 1
+        r = 2 * i + 2
+        if l < n and arr[i].duracion_estadia < arr[l].duracion_estadia:
+            largest = l
 
-    def merge_sort(self, list):
-        list_length = len(list)
-        if list_length == 1:
-            return list
-        mid_point = list_length // 2
-        left_partition = self.merge_sort(list[:mid_point])
-        right_partition = self.merge_sort(list[mid_point:])
-        return self.merge(left_partition, right_partition)
+        if r < n and arr[largest].duracion_estadia < arr[r].duracion_estadia:
+            largest = r
 
-    def merge(self, left, right):
-        output = []
-        i = j = 0
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                output.append(left[i])
-                i += 1
-            else:
-                output.append(right[j])
-                j += 1
-        output.extend(left[i:])
-        output.extend(right[j:])
-        return output
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
 
-manager = SistemaReservaciones()
-manager.cargar_datos("datos.json")
-manager.ordenar_reservaciones_por_rango
+            self.heapifyascendente(arr, n, largest)
+
+
+    def heapsortascendente(self, arr):
+        n = len(arr)
+
+        for i in range(n // 2 - 1, -1, -1):
+            self.heapifyascendente(arr, n, i)
+
+        for i in range(n - 1, 0, -1):
+            arr[i], arr[0] = arr[0], arr[i]
+            self.heapifyascendente(arr, i, 0)
+
+#METODO HEAPSORT PARA ORDENAR DE MANERA DESCENDENTE LAS RESERVACIONES SEGUN SU DURACION DE ESTADIA
+    def heapifydescendente(self, arr, n, i):
+        largest = i
+        l = 2 * i + 1
+        r = 2 * i + 2
+        if l < n and arr[i].duracion_estadia > arr[l].duracion_estadia:
+            largest = l
+
+        if r < n and arr[largest].duracion_estadia > arr[r].duracion_estadia:
+            largest = r
+
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+
+            self.heapifydescendente(arr, n, largest)
+
+
+    def heapsortdescendente(self, arr):
+        n = len(arr)
+
+        for i in range(n // 2 - 1, -1, -1):
+            self.heapifydescendente(arr, n, i)
+
+        for i in range(n - 1, 0, -1):
+            arr[i], arr[0] = arr[0], arr[i]
+            self.heapifydescendente(arr, i, 0)
+
+
+
+
+#MENU
+            
+manager = Funciones()
+manager.cargar_reservaciones_desde_json("datos.json")
+
+print("Menu de Ordenamiento de Reservaciones")
+print("-----------------Opciones-----------------")
+print("-Opcion 1:")
+print("-Opcion 4: Ordenar reservaciones según reservaciones realizadas")
+print("-Opcion 5: Ordenar reservaciones según su duración de estadía")
+opc = int(input("Ingrese la opcion de ordenamiento a elegir: "))
+#if opc == 1:
+
+#elif opc == 2:
+
+#elif opc == 3:
+
+#elif opc == 4:
+    
+if opc == 5:
+    print("Escoja la forma en que serán ordenadas las reservaciones")
+    print("1. Ascendente")
+    print("2. Descendente")
+    opc5 = int(input("Ingrese la opción a elegir: "))
+    if opc5 == 1:
+        manager.heapsortascendente(manager.reservaciones)
+        print("Las reservaciones ordenadas de forma ascendente en base a su duracion de estadia son las siguientes:")
+        manager.listar5()
+    elif opc5 == 2:
+        manager.heapsortdescendente(manager.reservaciones)
+        print("Las reservaciones ordenadas de forma ascendente en base a su duracion de estadia son las siguientes:")
+        manager.listar5()        
+                  
+else:
+    print("La opcion elegida no es correcta")
+
+
 
